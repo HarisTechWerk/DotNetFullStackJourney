7291bc650e9a1486f80b98d688dd1bc7fd1f2eba
Haris Qureshi <92890149+HarisTechWerk@users.noreply.github.com>
2025-02-27 03:15:54 +0100

    fix: move Program.cs to new Runner console app for execution

--- NotificationSystem/obj/Debug/net8.0/NotificationSystem.AssemblyInfo.cs
+++ NotificationSystem/obj/Debug/net8.0/NotificationSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NotificationSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddc1e2312a71d42e4b7c9fc8ea6f995d1a2ea091")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77074bc89283ec3b22eeab31eb5fd44fb9f2ac32")]
 [assembly: System.Reflection.AssemblyProductAttribute("NotificationSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NotificationSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
