115d2377a7b9434aa6ddd54d8f53cb1cf9eabb8a
Haris Qureshi <92890149+HarisTechWerk@users.noreply.github.com>
2025-02-27 19:13:02 +0100

    commit and corrected!

--- NotificationSystem/Services/ReverseTextTransformer.cs
+++ NotificationSystem/Services/ReverseTextTransformer.cs
@@ -1,18 +1,13 @@
 ﻿using NotificationSystem.Core;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NotificationSystem.Services
 {
     public class ReverseTextTransformer : ITextTransformer
     {
-        public string TranformText(string input)
+        public string TransformText(string input) 
         {
-            if (string.IsNullOrEmpty(input))
-                return string.Empty; 
+            if (string.IsNullOrEmpty(input)) 
+                return string.Empty;
 
             char[] chars = input.ToCharArray();
             Array.Reverse(chars);
